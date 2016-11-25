bf8472049837b7b903cfb69702aff06b1cabd6de
Alexander <alexander.tang@student.kuleuven.be>
2016-11-25 15:00:05 +0100

    crime and first time asylum data preprocessed individually

--- python_script/main/PreprocessCsvs.py
+++ python_script/main/PreprocessCsvs.py
@@ -1,12 +1,48 @@
 import pandas as pd
+import re
 
 raw_csv_directory = "../raw_csv_storage"
 preprocessed_csv_directory = "../preprocessed_csv_storage"
 
+
 def first_time_asylum_applicants():
     filename = "firstTimeAsylumApplicants.csv"
-    matrix = pd.read_csv(raw_csv_directory + "/" + filename, sep=',')
-    #TODO perform preprocessing
-    matrix.to_csv(path_or_buf=preprocessed_csv_directory + "/" + filename, index=False)
+    data = load_csv(filename)
+    del data['SEX']
+    del data['AGE'] # sex and age are always 'total', which makes them useless
+    data['Value'] = data['Value'].apply(format_number)
+    write_csv(data, filename)
 
-first_time_asylum_applicants()
+
+def crime():
+    filename = "crime.csv"
+    data = load_csv(filename)
+    data['Value'] = data['Value'].apply(format_number)
+    write_csv(data, filename)
+
+
+def format_number(numstring):
+    return re.sub("[^0-9.]", "", numstring)
+
+
+def load_csv(filename):
+    slash = "/"
+    if raw_csv_directory.endswith("/"):
+        slash = ""
+    return pd.read_csv(raw_csv_directory + slash + filename, sep=',')
+
+
+def write_csv(data, filename):
+    slash = "/"
+    if preprocessed_csv_directory.endswith("/"):
+        slash = ""
+    data.to_csv(path_or_buf=preprocessed_csv_directory + slash + filename, index=False)
+
+
+def perform_preprocessing():
+    crime()
+    first_time_asylum_applicants()
+
+
+
+perform_preprocessing()
